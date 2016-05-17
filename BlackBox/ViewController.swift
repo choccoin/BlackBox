94bb709b22012f6bab2b331ad7b390e6deedd431
王聪 <CXC@mac-mini.lan>
2016-05-17 21:59:10 +0800

    Initial Commit

---
+++ BlackBox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlackBox
+//
+//  Created by 王聪 on 16/5/17.
+//  Copyright © 2016年 AncStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
